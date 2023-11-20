4a9e52665515cea154f9ca6a7d880b4ed2ad781c
YvesJaques <ymoraisjaques@gmail.com>
2023-11-20 14:35:07 -0300

    Fix onMouseUp when releasing mouse outside canvas

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -47,7 +47,7 @@ class SketchPad {
                 this.#redraw();
             }
         }
-        this.canvas.onmouseup = () => {
+        document.onmouseup = () => {
             this.isDrawing = false
         }
         this.canvas.ontouchstart = (evt) => {
@@ -58,8 +58,8 @@ class SketchPad {
             const loc = evt.touches[0];
             this.canvas.onmousemove(loc);
         }
-        this.canvas.ontouchend = () => {
-            this.canvas.onmouseup();
+        document.ontouchend = () => {
+            document.onmouseup();
         }
         this.undoBtn.onclick = () => {
             this.paths.pop();
