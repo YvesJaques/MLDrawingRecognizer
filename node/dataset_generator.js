2af039d5019928ef1687e69e43dfabdd32b1b805
YvesJaques <ymoraisjaques@gmail.com>
2023-12-13 00:04:51 -0300

    Created generateImageFile function

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -3,6 +3,7 @@ const draw = require('../common/draw.js')
 const { createCanvas } = require('canvas');
 const canvas = createCanvas(400, 400);
 const ctx = canvas.getContext('2d');
+
 const constants = {};
 
 constants.DATA_DIR = "../data";
@@ -31,9 +32,15 @@ fileNames.forEach(fn => {
         });
 
         const paths = drawings[label];
-        fs.writeFileSync(constants.JSON_DIR + "/" + id + ".json",
-            JSON.stringify(drawings[label])
+        fs.writeFileSync(
+            constants.JSON_DIR + "/" + id + ".json",
+            JSON.stringify(paths)
         );
+
+        generateImageFile(
+            constants.IMG_DIR + "/" + id + ".png",
+            paths
+        )
 
         id++;
     }
@@ -42,3 +49,13 @@ fileNames.forEach(fn => {
 fs.writeFileSync(constants.SAMPLES,
     JSON.stringify(samples)
 );
+
+function generateImageFile(outFile, paths) {
+    ctx.clearRect(0, 0,
+        canvas.width, canvas.height
+    )
+    draw.paths(ctx, paths);
+
+    const buffer = canvas.toBuffer('image/png');
+    fs.writeFileSync(outFile, buffer);
+}
